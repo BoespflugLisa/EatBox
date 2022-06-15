7a8a1b076e728a9576875440717d7483076e6988
QuentinnBdr <101092905+QuentinnBdr@users.noreply.github.com>
2022-06-15 17:24:53 +0200

    Remove useless file and create DashBoard.vue

---
+++ frontend-vuejs/src/views/DashBoard.vue
@@ -0,0 +1,65 @@
+<template>
+
+    <div id="dashboard">
+
+
+        <div class="d-flex justify-center pt-7 ">
+
+            <h2>
+                Bonjour, {{ this.restaurantName }}
+
+            </h2>
+        </div>
+
+        <div class="d-flex justify-center pt-7">
+            <v-btn block>
+
+                Gestion des commandes
+            </v-btn>
+        </div>
+
+        <div class="d-flex  pt-7 ">
+
+            <h1>
+                Quelques statistiques...
+
+            </h1>
+        </div>
+
+        <div class="d-flex justify-center pt-7">
+
+            <v-card elevation="2" width="100%">
+
+                <v-card-text class="text-center font-weight-bold" >
+                    Nombres de commande ce mois-ci :
+                    <p class="text-xl-h1">{{ this.nbCommandes }}</p>
+                </v-card-text>
+
+            </v-card>
+        </div>
+
+        <div class="d-flex justify-center pt-7">
+            <v-btn block>
+
+                Gestion des commandes
+            </v-btn>
+        </div>
+
+
+    </div>
+
+
+</template>
+
+<script>
+export default {
+    data: function () {
+        return {
+            restaurantName: "Taco Bruno",
+            nbCommandes: 6,
+        }
+    },
+
+};
+</script>
+
