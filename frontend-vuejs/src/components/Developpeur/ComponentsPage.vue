b8f50e54331c4802d6d4aec3b3e2c7b66ac024d9
Lisa BOESPFLUG <lisa.boespflug@viacesi.fr>
2022-07-01 10:18:31 +0200

    Affichage des composants téléchargeables pour le dévelopeur (front)

--- frontend-vuejs/src/components/Developpeur/ComponentsPage.vue
+++ frontend-vuejs/src/components/Developpeur/ComponentsPage.vue
@@ -3,26 +3,19 @@
 
         <h1 class="mt-3">Liste des composants disponibles</h1>
 
-        <div class="mt-3" v-for="component in this.components" :key="component._id">
-            <v-card class="d-flex justify-space-between">
-                <div>
+        <div class="ma-3" v-for="component in this.components" :key="component._id">
+            <v-card>
                     <v-card-title>
-                        {{ component.Name}}
+                        {{ component.name }}
                     </v-card-title>
                     <v-card-text class="font-14">
-                        {{component.tipInstallation}}
+                        {{ component.tipInstallation }}
+                        <p class="code-template mt-1">{{component.installCommand}}</p>
                     </v-card-text>
-                </div>
-                <v-card-actions class="mr-3">
-                    <v-btn @click="showDetails(component)" rounded color="secondary">
-                        Détails
-                    </v-btn>
-                </v-card-actions>
             </v-card>
         </div>
 
     </div>
-
 
 
 </template>
@@ -32,22 +25,45 @@
 import {Component, Vue} from 'vue-property-decorator';
 
 @Component({
-    components: {
-    },
+    components: {},
 })
 
-export default class ComponentsPage extends Vue{
+export default class ComponentsPage extends Vue {
     name = "ComponentsPage"
 
     components = [{
-        _id: "526z5fev5645645",
-        Name: "listing des restaurants",
-    }]
+        _id: "526z5f58d8ev5645645",
+        name: "Restaurants",
+        tipInstallation: "Recupération de la liste des restaurants",
+        installCommand: "npm install eatbox-restaurants-list"
+    },
+        {
+            _id: "526ldlded85v5645645",
+            name: "Menus",
+            tipInstallation: "Recupération des menus par restaurant",
+            installCommand: "npm install  eatbox-menus-by-restaurant-list"
+        },
+        {
+            _id: "526ldld5ed85v8dok7d",
+            name: "Articles",
+            tipInstallation: "Recupération des articles par restaurant",
+            installCommand: "npm install  eatbox-articles-by-restaurant-list"
+        },
+    ]
 
 
 }
 </script>
 
 <style scoped>
+
+.code-template{
+    width: 100%;
+    background-color: #f7f8ff;
+    font-family: monospace;
+    padding: 2%;
+    color: darkblue;
+
+}
 
 </style>
